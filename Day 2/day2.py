485684677ff234464a1396db2faa9c6cb895f3d6
AryaShevaSatriaD <muryani060678@gmail.com>
2024-06-06 21:05:23 +0700

    Create day2.py

---
+++ Day 2/day2.py
@@ -0,0 +1,9 @@
+#meminta inputan user
+nama = input('Masukan Nama : ')
+nim = input('Masukan NIM : ')
+kelas = input('Masukan Kelas : ')
+
+#mencetak inputan dari user
+print(f'Nama saya {nama}')
+print(f'NIM saya {nim}')
+print(f'Dari kelas {kelas}')
