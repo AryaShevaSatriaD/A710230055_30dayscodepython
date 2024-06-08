3f6bac7caaa7d38b89914481e874b69888daf0c6
AryaShevaSatriaD <muryani060678@gmail.com>
2024-06-08 19:32:34 +0700

    Create day4.py

---
+++ Day 4/day4.py
@@ -0,0 +1,8 @@
+string = "Saya adalah manusia"
+
+print("panjang string = ", len(string))
+print("index pertama huruf f berada di ", string.index("s"))
+print("huruf a muncul sebanyak ", string.count("a"))
+print(string.upper())
+print(string.lower())
+print(string.split(" "))
