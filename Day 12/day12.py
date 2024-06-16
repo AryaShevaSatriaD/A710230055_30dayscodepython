612fe1aa9a53760f2127e629b2b843663c0cbfb1
AryaShevaSatriaD <muryani060678@gmail.com>
2024-06-16 19:59:23 +0700

    Create day12.py

---
+++ Day 12/day12.py
@@ -0,0 +1,6 @@
+try:
+    bilangan = int(input("Masukan bilangan bulat : "))
+    kuadrat = bilangan ** 2
+    print(f"kudrat dari {bilangan} adalah {kuadrat}")
+except:
+    print("input yang dimasukan tidak valid")
